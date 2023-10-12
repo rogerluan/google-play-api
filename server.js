ca95c2e047c6629cf168f9ca4fc51fd887483a22
Roger Oba <rogerluan.oba@gmail.com>
2023-10-12 00:38:23 -0300

    Convert to use ES Modules, latest scraper, and lint code.

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-const Express = require('express');
-const router = require('./lib');
+import Express from "express";
+import router from "./lib/index.js";
 
 const app = Express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/', router);
+app.use("/api/", router);
 
-app.get('/', function(req, res){
-  res.redirect('/api');
+app.get("/", function (req, res) {
+  res.redirect("/api");
 });
 
 app.listen(port, function () {
-  console.log('Server started on port', port);
+  console.log("Server started on port", port);
 });
